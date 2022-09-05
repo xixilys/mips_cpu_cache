98641a49bca183dc220ec58da1719c4f659f7eee
lys <1660509539@qq.com>
2022-09-05 23:14:56 +0800

    nscscc_2022 final submission

---
+++ src/out/production/main/mips_cpu_2nd/README.md
@@ -0,0 +1,2 @@
+# mips_cpu_2nd
+mips cpu with double issues and out of order  
---
+++ src/test/scala/examples/ANDtest.scala
@@ -0,0 +1,12 @@
+package test
+import chisel3._
+
+
+class ANDtest extends App{
+
+  import chisel3.stage.{ChiselStage, ChiselGeneratorAnnotation}
+
+  //(new chisel3.stage.ChiselStage).execute(args, Seq(ChiselGeneratorAnnotation(() => new AND)))
+
+}
+
